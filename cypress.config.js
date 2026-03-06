ee06b321f320234d12cf8a5771e04ed40ae7cfeb
mable@z.com <root@k.com>
2026-03-06 22:18:34 +0300

    sprint6

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:5173/react',
     async setupNodeEvents(on, config) {
       const { register } = await import('ts-node');
       register({
